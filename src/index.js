5a8a8b4fce4ef887a325d42d472040c416ffd184
danocide <dan.c.reilly@gmail.com>
2023-01-25 20:18:40 -0500

    added inquirer prompts

--- src/index.js
+++ src/index.js
@@ -1,29 +1,195 @@
 // require all of your classes
+const employee = require('./lib/Employee.js')
+const engineer = require('./lib/Engineer.js')
+const intern = require('./lib/Intern.js')
+const manager = require('./lib/Manager.js')
 // require inquirer, path if needed, fs
+const inquirer = require('inquirer');
+const fs = require('fs')
 
 // require your page template
-
+// const pageTemplate = require('./page-template.js')
 // empty team members array
-// empty id array
 
+let team = []
+
+// empty id array
+let teamId = []
 
 // function that wraps everything (like an init function)
+function init() {
+    const managerPrompt = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Managers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Managers ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Managers email?'
+        },
+        {
+            type: 'input',
+            name: 'phone',
+            message: 'What is the Managers office phone number?'
+        }
+    ];
 
-  // function for creating a manager (call this at the bottom of your init function)
-    // inquirer prompt with the manager questions
-    // in your .then - set up a variable for manager that is equal to a new instance of your Manager class passing in the responses you recieved from the user's input
-      // push that variable to your team members array, push the id to the id array
-      // call your next function
+    const rolePrompt = [
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What role would you like to add next?',
+            choices: ['Employee', 'Engineer', 'Intern']
+        }
+    ];
 
-  // next function should be for creating the team
+    const employeePrompt = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Employees name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Employees ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Employees email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the Employees Github username?'
+        }
+    ];
+
+    const engineerPrompt = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Engineers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Engineers ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Engineers email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the Engineers Github username?'
+        }
+    ];
+
+    const internPrompt = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Interns name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Interns ID number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Interns email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school did the Intern go to?'
+        }
+    ];
+
+    const createTeam = async () => {
+        // Get team manager information
+        const manager = await inquirer.prompt(managerPrompt);
+        team.push(manager);
+        teamId.push(manager.id);
+
+        while (true) {
+            // Get next role to add
+            const { role } = await inquirer.prompt(rolePrompt);
+
+            if (role === 'Employee') {
+                // Get employee information
+                const employee = await inquirer.prompt(employeePrompt);
+                employee.role = role;
+                team.push(employee);
+                teamId.push(employee.id);
+            } else if (role === 'Engineer') {
+                // Get engineer information
+                const engineer = await inquirer.prompt(engineerPrompt);
+                engineer.role = role;
+                team.push(engineer);
+                teamId.push(engineer.id);
+            } else if (role === 'Intern') {
+                // Get intern information
+                const intern = await inquirer.prompt(internPrompt);
+                intern.role = role;
+                team.push(intern);
+                teamId.push(intern.id);
+            }
+
+            // Ask if user wants to add another team member
+            const { addAnother } = await inquirer.prompt({
+                type: 'confirm',
+                name: 'addAnother',
+                message: 'Would you like to add another team member?'
+            });
+
+            if (!addAnother) {
+                break;
+            }
+        }
+
+        // Print out final team
+        console.log(team);
+        console.log(teamId);
+    };
+
+    createTeam();
+
+
+
+    // function teamManager() {
+    //     // function for creating a manager (call this at the bottom of your init function)
+    //     // inquirer prompt with the manager questions
+    //     // in your .then - set up a variable for manager that is equal to a new instance of your Manager class passing in the responses you recieved from the user's input
+    //     // push that variable to your team members array, push the id to the id array
+    // }
+
+
+    // call your next function
+
+    // next function should be for creating the team
     // this should ask the user what type of employee they would like to create
     // in your .then, have a conditional or switch case that runs that employee types function based on what they choose
     // or if they select the option that says they are done adding team members, run the function that builds the team
 
 
-    function buildTeam() {
-        // fs.writeFileSync(yourPathToDistFolder, functionFromPageTemplate(teamMembers), 'utf-8');
-      }
-  
-  
+    // function buildTeam() {
+    //     // fs.writeFileSync(yourPathToDistFolder, functionFromPageTemplate(teamMembers), 'utf-8');
+    // }
+};
+
+init();
   // make sure call your init function
---
+++ src/page-template.js
@@ -0,0 +1,77 @@
+return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <nav class="navbar bg-danger bg-body-tertiary py-5">
+        <div class="container-fluid justify-content-center">
+            <span class="h1 text-white">My Team</span>
+        </div>
+    </nav>
+
+    <div class="container-md">
+        <div class="row justify-content-center">
+
+            <div class="col-4 card mt-5 mx-2" style="width: 18rem;">
+                <div class="bg-primary card-body text-white h2">
+                    <h5 class="card-title">Leroy Jenkins</h5>
+                    <p class="card-text">🤡 Manager</p>
+                </div>
+                <ul class="list-group list-group">
+                    <li class="list-group-item">ID:</li>
+                    <li class="list-group-item">Email: <a href="">email</a></li>
+                    <li class="list-group-item">Office Number: </li>
+                </ul>
+            </div>
+
+            <div class="col-4 card mt-5 mx-2" style="width: 18rem;">
+                <div class="bg-primary card-body text-white h2">
+                    <h5 class="card-title">ur mom</h5>
+                    <p class="card-text">🤬 Engineer</p>
+                </div>
+                <ul class="list-group list-group">
+                    <li class="list-group-item">ID:</li>
+                    <li class="list-group-item">Email: <a href="">email</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/Danocide">github</a></li>
+                </ul>
+            </div>
+
+            <div class="col-4 card mt-5 mx-2" style="width: 18rem;">
+                <div class="bg-primary card-body text-white h2">
+                    <h5 class="card-title">Chuck u Testa</h5>
+                    <p class="card-text">🛼 Employee</p>
+                </div>
+                <ul class="list-group list-group">
+                    <li class="list-group-item">ID:</li>
+                    <li class="list-group-item">Email: <a href="">email</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/Danocide">github</a></li>
+                </ul>
+            </div>
+
+            <div class="col-4 card mt-5 mx-2" style="width: 18rem;">
+                <div class="bg-primary card-body text-white h2">
+                    <h5 class="card-title">Tommy 2 Teeth</h5>
+                    <p class="card-text">👶 Intern</p>
+                </div>
+                <ul class="list-group list-group">
+                    <li class="list-group-item">ID:</li>
+                    <li class="list-group-item"><a href="">email</a></li>
+                    <li class="list-group-item">School: School of Rock</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>`
